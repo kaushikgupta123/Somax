2addfed02dba142af3fad03ce9312b8b3f78d82b
kaushik.gupta <kaushik.gupta@indusnet.co.in>
2025-06-09 15:47:23 +0530

    Localization

---
+++ DEV/Localization/LocalizationManager.cs
@@ -0,0 +1,26 @@
+﻿using DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+using Utility;
+using System.Linq;
+
+namespace Localization
+{
+    public class LocalizationManager
+    {
+        private Localizations _DcLoc = new Localizations();
+
+        private LookupCache _lookupCache = new LookupCache();
+        private MemoryCache _cache = MemoryCache.Default;
+
+        public List<Localizations> GetLocalization(DatabaseKey dbKey, String ResourceSet, String LocaleId)
+        {
+            _DcLoc.LocaleId = LocaleId;
+            _DcLoc.ResourceSet = ResourceSet;
+            var localizations = _lookupCache.GetOrAddCache<Localizations>(LookupCacheKeys.LOCALIZATION, () => _DcLoc.RetrieveByResourceSet(dbKey), _cache);
+
+            return localizations.ToList();
+        }
+    }
+}
---
+++ DEV/Localization/Localize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Localization
+{
+    public class Localize
+    {
+        #region public Variables
+        public long ClientId { get; set; }
+        public long LocalizationId { get; set; }
+        public string ResourceId { get; set; }
+        public string Value { get; set; }
+        public string LocaleId { get; set; }
+        public string ResourceSet { get; set; }
+        #endregion
+
+       
+    }
+}
