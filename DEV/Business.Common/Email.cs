a5b5f55674930670ff841c1b5a698a97d7d481e8
kaushik.gupta <kaushik.gupta@indusnet.co.in>
2025-06-09 15:39:18 +0530

    somaxmvcapp

---
+++ DEV/Business.Common/Email.cs
@@ -0,0 +1,59 @@
+﻿using Common.Constants;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Common
+{
+    public class Email
+    {
+        #region Properties
+        //public string Sender { get; set; }
+        //public List<string> Recipients { get; set; }
+        //public string Subject { get; set; }
+        //public string Body { get; set; }
+        //public bool IsBodyHtml { get; set; }
+        #endregion
+        #region Constructors
+        //public Email()
+        //{
+        //    Recipients = new List<string>();
+        //    IsBodyHtml = true; // IsBodyHtml is set to true by default.
+        //}
+        #endregion
+
+        #region Public Methods
+        //public void Send()
+        //{
+        //    // Id the Sender is empty, then the system email is used.
+        //    if (String.IsNullOrEmpty(Sender)) { Sender = ConfigurationManager.AppSettings[WebConfigConstants.SMTP_EMAIL]; }
+
+        //    MailMessage message = new MailMessage()
+        //    {
+        //        From = new MailAddress(Sender),
+        //        Subject = this.Subject,
+        //        Body = this.Body,
+        //        IsBodyHtml = this.IsBodyHtml
+        //    };
+
+        //    foreach (string recipient in Recipients)
+        //    {
+        //        message.To.Add(recipient);
+        //    }
+
+        //    SmtpClient smtp = new SmtpClient();
+        //    //smtp.Timeout = 10000;
+        //    //smtp.Host = ConfigurationManager.AppSettings[WebConfigConstants.SMTP_SERVER];
+        //    //smtp.Port = 2525;
+        //    //smtp.UseDefaultCredentials = false;
+        //    //smtp.Credentials = new NetworkCredential(ConfigurationManager.AppSettings[WebConfigConstants.SMTP_USER_NAME], ConfigurationManager.AppSettings[WebConfigConstants.SMTP_PASSWORD]);
+        //    smtp.Send(message);
+
+        //}
+        #endregion
+    }
+}
---
+++ DEV/Business.Common/Encryption.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Business.Common
+{
+    public class Encryption
+    {
+        //#region Member Variables
+        //private static byte[] m_seed = new byte[] { 0x4d, 0x61, 0x72, 0x73, 0x68, 0x61, 0x43, 0x68, 0x61, 0x69, 0x43, 0x68, 0x69 };
+        //#endregion
+
+        //#region Public Methods
+        //public static string EncryptLong(long data, string encrptKey)
+        //{
+        //    byte[] encryptedData = TripleDESEncrypt(data.ToString(), encrptKey);
+        //    return Convert.ToBase64String(encryptedData, 0, encryptedData.Length);
+        //}
+
+        //public static long DecriptLong(string data, string encrtyKey)
+        //{
+        //    string decryptedData = TripleDESDecrypt(Convert.FromBase64String(data), encrtyKey);
+        //    long result;
+        //    long.TryParse(decryptedData, out result);
+        //    return result;
+        //}
+
+        //public static string EncryptString(string data, string encrptKey)
+        //{
+        //    byte[] encryptedData = TripleDESEncrypt(data, encrptKey);
+        //    return Convert.ToBase64String(encryptedData, 0, encryptedData.Length);
+        //}
+
+        //public static string DecriptString(string data, string encrtyKey)
+        //{
+        //    string decryptedData = TripleDESDecrypt(Convert.FromBase64String(data), encrtyKey);
+        //    return decryptedData;
+        //}
+
+        ///// <summary>Encrypt the string with SHA512 algorithm</summary>
+        ///// <param name="strToHash">The string to be encrypted</param>
+        ///// <returns>The encrypted string</return>
+        //public static string SHA512Encrypt(string strToEncrypt)
+        //{
+        //    SHA512 sha = new SHA512Managed();
+        //    byte[] data = sha.ComputeHash(Encoding.ASCII.GetBytes(strToEncrypt));
+        //    string ret = BitConverter.ToString(data).Replace("-", "").ToLower();
+        //    return ret;
+        //}
+        //#endregion
+        
+        //#region Private Methods
+        //private static byte[] TripleDESEncrypt(string strToEncrypt, string encryptKey)
+        //{
+        //    MemoryStream ms = new MemoryStream();
+
+        //    byte[] dataToEncrypr = Encoding.Unicode.GetBytes(strToEncrypt);
+
+        //    PasswordDeriveBytes pdb = new PasswordDeriveBytes(encryptKey, m_seed);
+
+        //    TripleDES tripleDes = TripleDES.Create();
+        //    tripleDes.Key = pdb.GetBytes(24);
+        //    tripleDes.IV = pdb.GetBytes(8);
+
+        //    CryptoStream cs = new CryptoStream(ms, tripleDes.CreateEncryptor(), CryptoStreamMode.Write);
+        //    cs.Write(dataToEncrypr, 0, dataToEncrypr.Length);
+        //    cs.Close();
+        //    tripleDes.Clear();
+
+        //    return ms.ToArray();
+        //}
+
+        //private static string TripleDESDecrypt(byte[] encryptedData, string encryptKey)
+        //{
+        //    try
+        //    {
+        //        MemoryStream ms = new MemoryStream();
+
+        //        PasswordDeriveBytes pdb = new PasswordDeriveBytes(encryptKey, m_seed);
+
+        //        TripleDES tripleDes = TripleDES.Create();
+        //        tripleDes.Key = pdb.GetBytes(24);
+        //        tripleDes.IV = pdb.GetBytes(8);
+
+        //        CryptoStream cs = new CryptoStream(ms, tripleDes.CreateDecryptor(), CryptoStreamMode.Write);
+        //        cs.Write(encryptedData, 0, encryptedData.Length);
+        //        cs.Close();
+        //        tripleDes.Clear();
+
+        //        string decryptdText = System.Text.Encoding.Unicode.GetString(ms.ToArray());
+        //        return decryptdText;
+        //    }
+        //    catch
+        //    {
+        //        return null;
+        //    }
+        //}
+        //#endregion
+        
+    }
+}
